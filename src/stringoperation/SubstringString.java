7bfa2408e5683b8eafe2b2e28732949c2d9cc42c
chowsumit <50622513+chowsumit@users.noreply.github.com>
2023-07-30 12:16:27 -0400

    Create SubstringString.java

---
+++ src/stringoperation/SubstringString.java
@@ -0,0 +1,18 @@
+
+public class SubstringString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String str = "Hello world!";
+	
+	System.out.println(str);
+	
+	System.out.println(str.substring(2)); //llo world! (start from 2nd index to end)
+	
+	System.out.println(str.substring(6, 9)); //wor (start from 6th index to 9th index excluding the letter of 9th index)
+	
+	
+	}
+
+}
