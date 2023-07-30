474203bd6030fe8f693ef527e9176e5b8f73a831
chowsumit <50622513+chowsumit@users.noreply.github.com>
2023-07-30 12:53:09 -0400

    Update ReplaceOperationString.java

--- src/stringoperation/ReplaceOperationString.java
+++ src/stringoperation/ReplaceOperationString.java
@@ -4,11 +4,19 @@ public class ReplaceOperationString {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String str = "Hello world!";
+		String str1 = "Hello world!";	
 		
-		String newStr = str.replace("l", "x");
 		
-		System.out.println(newStr);
+		String newStr_word = str1.replace("world", "earth");
+		
+		System.out.println(newStr_word);
+		
+		String str2 = "Hello world!";	
+		
+		
+		String newStr_letter = str1.replace("l", "x");
+		
+		System.out.println(newStr_letter);
 
 	}
 
