3b58cbf60482a094f28fc51fd05c234c69ae8226
chowsumit <50622513+chowsumit@users.noreply.github.com>
2023-07-30 12:36:49 -0400

    Create ReplaceOperationString.java

---
+++ src/stringoperation/ReplaceOperationString.java
@@ -0,0 +1,15 @@
+
+public class ReplaceOperationString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str = "Hello world!";
+		
+		String newStr = str.replace("l", "x");
+		
+		System.out.println(newStr);
+
+	}
+
+}
